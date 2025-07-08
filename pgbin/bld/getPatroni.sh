890ad2758bf8ed9e66256fbe4068bc6a910d56f9
Muhammad Aqeel <muhammad.aqeel@pgedge.com>
2025-07-08 16:28:46 +0500

    [BR-16]: Updates getPatroni.sh to get Patroni source code from upstream repository.

--- pgbin/bld/getPatroni.sh
+++ pgbin/bld/getPatroni.sh
@@ -1,17 +1,16 @@
 set -x
 
-ver=3.2.2.2
+ver=4.0.6
 minor_ver=1
-
 
 in_file=v$ver.tar.gz
 out_dir=patroni-$ver-$minor_ver
 
 rm -rf $in_file*
 
-wget https://github.com/pgEdge/pgedge-patroni/archive/refs/tags/$in_file
+wget https://github.com/patroni/patroni/archive/refs/tags/$in_file
 tar -xf $in_file
-mv pgedge-patroni-$ver $out_dir
+mv patroni-$ver $out_dir
 tar czf $out_dir.tgz $out_dir
 
 rm -r $out_dir
